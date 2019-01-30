cf0918e1b27b4ae5ba0e4c42f650fad95d21ffed
ADHSoft <adhsoft0@gmail.com>
2019-01-30 00:25:38 -0300

    vci

--- vceconverter/structss.h
+++ vceconverter/structss.h
@@ -1,10 +1,10 @@
 #ifndef STRUCTSS_H
 #define STRUCTSS_H
 
-
 struct header_s
 {
-    char header[4],vcmf_version[4];
+    char header[4];
+    unsigned char version[4];
     unsigned long doctype;//?
     unsigned long fps;
     unsigned long maxkey;
@@ -14,18 +14,10 @@ struct header_s
 
 typedef struct header_s header_t;
 
-struct layer_s
-{
-    unsigned long texcnt;
-
-};
-
-typedef struct layer_s layer_t;
-
 struct texture_s
 {
     char texname[0x70];
-    char unk[0x1c];
+    unsigned char texcoord[0x1c];
 
     //str: texname only=0x80
 
@@ -44,7 +36,7 @@ struct anikey_s
     float uvs2[2];
     float points[8]; //order: x1,x2,x3,x4,y1,y2,y3,y4
     float unk2;
-    float texid;//aka
+    float tex_id;
     unsigned long mode;
     float inccount;//as appears in rostr viewer
     float color[4]; //rgba
